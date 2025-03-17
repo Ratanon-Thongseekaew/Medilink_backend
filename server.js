dd076ba37ba061e343cd3e5370444a682175962e
naraphan13 <thongnit2201@gmail.com>
2025-03-17 10:56:46 +0700

    change port

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use("/api/user", userRouter);
 app.use(handleError);
 
 // Start Server
-const PORT = 8889;
+const PORT = 8888;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
