3fa8bb1fb972d47076f5024fcd02d72c8780149a
naraphan13 <thongnit2201@gmail.com>
2025-03-18 14:59:22 +0700

    ai and sendemail

--- src/routes/ai-router.js
+++ src/routes/ai-router.js
@@ -1,51 +1,13 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-
-const authRouter = require("./src/routes/auth-router");
-const doctorRouter = require("./src/routes/doctor-router");
-const hospitalRouter = require("./src/routes/hospital-route");
-const programRouter = require("./src/routes/program-router");
-const userRouter = require("./src/routes/user-router");
-const appointmentRouter = require("./src/routes/appointment-router");
-const scheduleRouter = require("./src/routes/schedule-router");
-const handleError = require("./src/middlewares/handleError");
-// test
-
+const aiRouter = express.Router();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const prisma = require("../configs/prisma");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const prisma = require("./src/configs/prisma");
-const aiRouter = require("./src/routes/ai-router");
-// test
-const app = express();
-
-// Middlewares
-app.use(cors()); // Allows cross domain
-app.use(morgan("dev")); // Show log terminal
-app.use(express.json()); // For read json
-
-// // Routing
-app.use("/api/auth", authRouter);
-app.use("/api/hospital", hospitalRouter);
-app.use("/api/program", programRouter);
-app.use("/api/appointment", appointmentRouter);
-app.use("/api/schedule", scheduleRouter);
-app.use("/api/doctor", doctorRouter);
-app.use("/api/user", userRouter); 
-app.use("/api/ai", aiRouter); 
-
-// app.use('/api',userRouter)
 
 
 
 
-// test
-
-
-
-
-app.post("/ask", async (req, res) => {
+aiRouter.post("/ask", async (req, res,next) => {
     try {
         const { question } = req.body;
 
@@ -150,9 +112,7 @@ app.post("/ask", async (req, res) => {
 
 
 
-
-// ✅ API: /chat1 - วิเคราะห์อาการเบื้องต้น และแนะนำแพทย์
-app.post("/chat1", async (req, res) => {
+aiRouter.post("/chat", async (req, res,next) => {
     try {
         const { symptom, duration, underlyingCondition, currentAddress } = req.body;
         console.log('req.body', req.body);
@@ -199,7 +159,7 @@ app.post("/chat1", async (req, res) => {
         console.log('Response from /chat1:', text);
 
         // 🔹 เรียก API /ask โดยใช้คำตอบจาก /chat1
-        const askResponse = await fetch("http://localhost:8888/ask", {
+        const askResponse = await fetch("http://localhost:8888/api/ai/ask", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -221,29 +181,7 @@ app.post("/chat1", async (req, res) => {
 
 
 
-// test
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Handle errors
-app.use(handleError);
-
-// Start Server
-const PORT = 8888;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+module.exports = aiRouter;
